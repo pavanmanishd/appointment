be6b48c814277dbab0885c364349685a6dc10149
pavanmanishd <pavanmanishd@gmail.com>
2024-10-22 19:28:28 +0530

    redirect to respective dashboard on authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,29 @@
+"use client";
 import { LandingPageComponent } from "@/components/landing-page"; 
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [role, setRole] = useState<string | null>(null);
+  const [token, setToken] = useState<string | null>(null);
+
+  useEffect(() => {
+    const user = localStorage.getItem('user');
+    if (user) {
+      const { role, token } = JSON.parse(user);
+      setRole(role);
+      setToken(token);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (role === 'admin') {
+      window.location.href = '/admin';
+    } else if (role === 'beautician') {
+      window.location.href = '/beautician';
+    } else if (role === 'client') {
+      window.location.href = '/client';
+    }
+  }, [role]);
+
   return <LandingPageComponent />;
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,8 +2,22 @@
 
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
+import { useState, useEffect } from "react";
 
 export function Navbar() {
+    const handleLogout = () => {
+        localStorage.removeItem('user');
+        window.location.href = '/';
+    }
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    useEffect(() => {
+        const storedUser = localStorage.getItem('user');
+        if (storedUser) {
+            setIsLoggedIn(true);
+        }
+    }, []);
+            
+
     return (
         <header className="px-4 lg:px-6 h-14 flex items-center">
             <Link className="flex items-center justify-center" href="#">
@@ -11,16 +25,8 @@ export function Navbar() {
                 <span className="h-6 w-6 text-2xl">💅</span>
             </Link>
             <nav className="ml-auto flex gap-4 sm:gap-6">
-                <Link className="text-sm font-medium hover:underline underline-offset-4" href="#">
-                    Book Appointment
-                </Link>
-                <Link className="text-sm font-medium hover:underline underline-offset-4" href="#">
-                    My Appointments
-                </Link>
-                <Link className="text-sm font-medium hover:underline underline-offset-4" href="#">
-                    Profile
-                </Link>
-                <Button variant="outline">Logout</Button>
+                {isLoggedIn && 
+                <Button variant="outline" onClick={handleLogout}>Logout</Button>}
             </nav>
         </header>
     )
